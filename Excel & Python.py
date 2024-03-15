ce664d1262ac1620cddf405b7546181e8c6b1e9e
ParthP1803 <163509847+ParthP1803@users.noreply.github.com>
2024-03-15 09:34:18 +0100

    Update Excel & Python.py

--- Excel & Python.py
+++ Excel & Python.py
@@ -5,6 +5,8 @@ from scipy.stats import *
 import xlwings as xw
 import random as r
 
+################################################################################################################
+# Code to link python with Excel sheets
 wb = xw.Book('Optimal.xlsx')
 WACC = wb.sheets['WACC']
 input_sheet = wb.sheets['Cost of Capital Schedule']
@@ -12,6 +14,8 @@ sheet4 = wb.sheets['Sheet4']
 df_spread = wb.sheets['Default spread']
 historical_data = wb.sheets['Historical DAta']
 
+################################################################################################################
+# Bootstrap part of the code - To find the distribution of EBIT/Pre WC-CFO multiple
 # Bootstrap function
 def bootstrap(data,n_samples,statfunc):
     data = np.array(data)
@@ -32,13 +36,15 @@ print('Empirical mean: %.4f' %np.mean(bootstrap_data))
 print('99% Confidence interval : ',conf_int)
 print('Standard deviation: %.5f'%(np.std(bootstrap_data)))
 
-# Plotting bootstrapped data histogram
-plt.hist(bootstrap_data,bins=50,color='g',edgecolor='black',alpha=0.7)
-plt.title('EBIT-Cash coverage multiple')
-plt.ylabel('Frequency',fontsize=8)
-plt.tight_layout()
-plt.show()
+# Plotting bootstrapped data histogram (YOU CAN COMMENT THE BELOW CODE OUT TO SEE THE BOOTSTRAP DISTRIBUTION)
 
+# plt.hist(bootstrap_data,bins=50,color='g',edgecolor='black',alpha=0.7)
+# plt.title('EBIT-Cash coverage multiple')
+# plt.ylabel('Frequency',fontsize=8)
+# plt.tight_layout()
+# plt.show()
+################################################################################################################
+# Main part of the code
 
 #Memoization & Iteration_default_spread
 cache = {}
@@ -53,9 +59,9 @@ def add_spread(rating):
                 spread = df_spread.cells(i,'F').value
                 cache[rating] = spread
                 return spread
-
+                
+# Function to assign distributions to inputs (Risk-free rate, ERP, CFO-Pre WC)
 def change_inputs():
-    # Distributions of inputs (Risk-free rate, ERP, CFO-Pre WC)
     global risk_free
     risk_free = r.triangular(0.0375,0.055,0.04) # risk-free rate
     equity_risk_prm = skewnorm.rvs(-3,loc=0.055,scale=0.0055)  # ERP - Source: Kroll report
@@ -72,22 +78,22 @@ def pre_tax_cod_iteration():
         list_rate_iter_1.append(risk_free + real_spread)
     sheet4.range('E10:M10').value = list_rate_iter_1
 
-def expected_value(index,lst): # the index represents debt level - For ex, 2 for 20%
+def expected_value(index,lst): # the index represents debt level - For ex., 2 for 20%
     lst_1 = []
     for i in lst:
         lst_1.append(i[index])
     return np.mean(lst_1) # to get the expected WACC at debt level (index)
 
-
 lst_outputs = []
 #Simulations
-for i in range(10000): # Just change the range to number of simulations you would like to have
+for i in range(10000): # Just change the range to the number of simulations you would like to have
     change_inputs()
     sheet4.range('E10:M10').options(transpose=True).value = 0.01
     for j in range(3):
         pre_tax_cod_iteration()
     lst_outputs.append(input_sheet.cells(27,'G').value)
 
+# Output Distribution of WACC at 60% Leverage ratio
 plt.hist(lst_outputs,bins=50,color='blue',range=[0.05,0.1], density=True, edgecolor='black')
 plt.title('Distribution - WACC at 60% leverage ratio')
 plt.ylabel('Frequency',fontsize=8)
@@ -96,15 +102,4 @@ plt.show()
 print(np.mean(lst_outputs), np.std(lst_outputs))
 print(np.percentile(lst_outputs,[0,10,20,30,40,50,60,70,80,90]))
 print(np.percentile(lst_outputs,[2.5,97.5]))
-
-# lst = []
-# for i in range(100000):
-#     date = skewnorm.rvs(3.5,loc=750,scale=40)
-#     lst.append(date)
-# plt.hist(lst,bins=50,color='g',edgecolor='black',alpha=0.7)
-# plt.title('Pre-WC CFO')
-# plt.ylabel('Frequency',fontsize=8)
-# plt.tight_layout()
-# plt.show()
-# print(np.mean(lst),np.std(lst))
-# print(np.percentile(lst,[0.5,99.5]))
+########################################################################################################################
